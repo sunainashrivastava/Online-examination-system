c3be1da48a4731c90d4cf62bfe35a120b8b035b2
sunainashrivastava <sushrivastava@bestpeers.com>
2018-07-24 14:34:12 +0530

    commit

--- app/controllers/take_tests_controller.rb
+++ app/controllers/take_tests_controller.rb
@@ -1,24 +1,24 @@
 class TakeTestsController < ApplicationController
-	before_action :authenticate_user!
-	load_and_authorize_resource
-	# skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+  load_and_authorize_resource
+  skip_before_action :verify_authenticity_token
 
-	def new
-		@take_test = TakeTest.new
-		@take_test.pictures.build
-	end
+  def new
+    @take_test = TakeTest.new
+    @take_test.pictures.build
+  end
 
-	def questions
-		@questions = TakeTest.where(type: params[:type])	
-	end
+  def questions
+    @questions = TakeTest.where(type: params[:type])  
+  end
 
-	def create
-		@take_test = TakeTest.new(take_test_params)
-		if @take_test.save
-   	  redirect_to 'show'
-   	else
-   		render 'new'
-   	end
+  def create
+    @take_test = TakeTest.new(take_test_params)
+    if @take_test.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -31,14 +31,13 @@ class TakeTestsController < ApplicationController
   def destroy
     @take_test = TakeTest.find(params[:id])
     @take_test.destroy
- 
     redirect_to questions_path
   end
 
-	private
-	def take_test_params
-	  params.require(:take_test).permit(:type, :sno, :question, :op1, :op2,
-	  	:op3, :op4, :ans, :pictures_attributes => [:avatar])
-	end
+  private
+  def take_test_params
+    params.require(:take_test).permit(:type, :sno, :question, :op1, :op2,
+      :op3, :op4, :ans, :pictures_attributes => [:avatar])
+  end
 end
 
