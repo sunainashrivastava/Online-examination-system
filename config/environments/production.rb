fcfc011354185f5508c91b93e078d745f9090113
sunainashrivastava <sushrivastava@bestpeers.com>
2018-08-20 16:36:28 +0530

    edit production.rb for mail configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,16 +80,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for mailgun
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
+   :address => 'smtp.sendgrid.net',
   :port => 587,
-  :domain => 'enigmatic-shelf-72361.herokuapp.com',
+  :domain => 'herokuapp.com',
   :user_name => 'shirivastavasunaina@gmail.com',
   :password => 's12345678s',
   :authentication => :plain,
   :enable_starttls_auto => true
 }
 
-config.action_mailer.default_url_options = { :host => 'enigmatic-shelf-72361.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'enigmatic-shelf-72361.herokuapp.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
