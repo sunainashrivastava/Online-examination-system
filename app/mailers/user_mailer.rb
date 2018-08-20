46a56db592a8a887ac4d3204cc263fa8d2f95ed2
sunainashrivastava <sushrivastava@bestpeers.com>
2018-08-20 17:40:16 +0530

    edit production.rb for mail configuration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,6 @@
 # class UserMailer
 class UserMailer < ApplicationMailer
   default from: 'online_examination_system@oes.com'
-  require 'sendgrid-ruby'
-  include SendGrid
 
   def score_email(user, score)
     @user = user
